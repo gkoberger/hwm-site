b804089781c89a4f547d0f83c2f8700de5ed4962
Gregory Koberger <gkoberger@gmail.com>
2012-06-14 16:19:25 -0700

    Woops, forgot the important part

---
+++ button.php
@@ -0,0 +1,7 @@
+  <a href="#" id="install">Get Started!</a>
+  <div class="cap">No browser restart required.</div>
+  <div id="unsupported">
+    <a href="http://firefox.com" target="_blank"><img src="img/firefox.png"></a>
+    <a href="http://google.com/chrome" target="_blank"><img src="img/chrome.png"></a>
+    <strong>Only available for <a href="http://firefox.com" target="_new">Firefox 4+</a> and <a href="http://google.com/chrome" target="_new">Chrome 18+</a></strong>
+  </div>
