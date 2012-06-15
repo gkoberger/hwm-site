e45347baa33f2c41fbe22cacdead5128c266159d
Gregory Koberger <gkoberger@gmail.com>
2012-06-14 18:30:43 -0700

    Not that soon

--- js/script.js
+++ js/script.js
@@ -8,17 +8,17 @@ var installButton = function() {
         $('.install').attr('href', '/hwm.xpi');
         $('.install').click(function() {
             $(this).addClass('disabled').text('Installing...');
+            $('#restart').hide();
+            $('#already_installed').show();
         });
-        $('#restart').hide();
-        $('#already_installed').show();
     } else if (!!(window.chrome && chrome.webstore && chrome.webstore.install) && parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2]) >= 18) {
         $('.install').click(function() {
             var url = $('link[rel=chrome-webstore-item]').attr('href');
             chrome.webstore.install(url, installSuccess, installNope);
             $(this).addClass('disabled').text('Installing...');
+            $('#restart').hide();
+            $('#already_installed').show();
         });
-        $('#restart').hide();
-        $('#already_installed').show();
     } else {
         $('#unsupported').show();
         $('#restart').hide();
